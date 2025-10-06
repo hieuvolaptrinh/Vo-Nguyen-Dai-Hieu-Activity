dbc5737d9751760022c9ff56cda79cf59b991848
hieuvolaptrinh <vndhieuak@gmail.com>
2025-10-06 18:51:47 +0700

    done

--- app/src/main/java/com/example/vonguyendaihieu/ProfileActivity.java
+++ app/src/main/java/com/example/vonguyendaihieu/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.vonguyendaihieu;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ProfileActivity extends AppCompatActivity {
-
+TextView txtName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +21,9 @@ public class ProfileActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        txtName=findViewById(R.id.txtName);
+        String name=getIntent().getStringExtra("username");
+        txtName.setText(name);
+
     }
 }
